432224c9629b9438c23d2c301d469c4fcfe6ed86
sujonifty <sujonifty@gmail.com>
2024-05-28 00:16:37 +0600

    delete  section completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app= express();
 const port = process.env.PORT || 5000;
@@ -47,7 +47,15 @@ async function run() {
     })
 
     app.get('/carts', async(req, res)=>{
-      const result = await cartsCollection.find().toArray();
+      const UserEmail= req.query.email;
+      const query ={email: UserEmail};
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+  })
+    app.delete('/carts/:id', async(req, res)=>{
+      const id= req.params.id;
+      const query ={_id: new ObjectId(id)};
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
   })
     // Send a ping to confirm a successful connection
